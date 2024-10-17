e3f1db31440ce3548eccff43e3748921b566d169
Pedro Hurtado <pedrohurt@gmail.com>
2024-10-17 09:54:30 +0200

    webclient ejercicio

--- demo/src/main/java/com/example/demo/Infraestructure/RepositoryIngredientImpl.java
+++ demo/src/main/java/com/example/demo/Infraestructure/RepositoryIngredientImpl.java
@@ -9,7 +9,9 @@ import reactor.core.publisher.Mono;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.ArrayList;
 @Component
@@ -21,12 +23,21 @@ public class RepositoryIngredientImpl implements RepositoryIngredient{
         ingredients.add(entity);
     }
     @Override    
-    public Mono<Ingredient> get(UUID id) {        
-       var result =  ingredients.stream().filter(i->i.getId().equals(id)).findFirst();       
+    public Mono<Ingredient> get(UUID id) {     
+
+       WebClient client = WebClient.create("https://super-chainsaw-vp7g5r67w6hxv5j-3000.app.github.dev");
+
+       return  client.get()
+		.uri("/ingredients/{id}", id)
+        .accept(MediaType.APPLICATION_JSON)        
+		.retrieve()        
+		.bodyToMono(Ingredient.class);
+
+       /*var result =  ingredients.stream().filter(i->i.getId().equals(id)).findFirst();       
        if(!result.isPresent()) {
         return Mono.empty();
        }
-       return Mono.just(result.get());
+       return Mono.just(result.get());*/
     }
     @Override
     public Flux<Ingredient> getAll() {
